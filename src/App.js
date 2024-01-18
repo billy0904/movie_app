352aaa4e604be9a8a10aa950691ed7ebfdbbd5f9
Gabin Lee <gb94@ewhain.net>
2024-01-18 14:24:32 +0900

    #10.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Billy from "./Billy";
 
+function Food({ fav }) {
+  return <h1>{fav}도 먹고싶고</h1>;
+}
 
 function App() {
   return (
     <div className="App">
-      <h3>This is h3</h3>Hello, h3!<Billy />
+      <h1>너무 배고픈데</h1>
+      <Food fav="닭갈비" />
+      <Food fav="마라탕" />
+      <Food fav="순대국" />
+      <Food fav="마제소바" />
+      <h1>그냥 전부 내 입으로.</h1>
     </div>
   );
 }
